85f77cb6a307f15c89a1e3b7c7ccce4fded0ad64
HugoHLZhang <hugo.zhang@edu.esiee.Fr>
2023-12-26 21:22:22 +0800

    update all html pages

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # French-Resort-FrontEnd
+
+conda create -n french_resort python=3.11
+conda activate french_resort 
+pip install -r requirements.txt
--- src/app.py
+++ src/app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/mybookings')
+def mybookings():
+    return render_template('mybookings.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
