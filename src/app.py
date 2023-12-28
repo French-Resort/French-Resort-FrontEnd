8cbf19a55f9cf6e283e9e1168a66a27c7b606795
HugoHLZhang <hugo.zhang@edu.esiee.Fr>
2023-12-29 04:00:36 +0800

    Update a lot

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, SelectField
+from wtforms import StringField, SubmitField, DateField, SelectField, EmailField
 from wtforms.widgets import PasswordInput
 from wtforms.validators import DataRequired, ValidationError, InputRequired, Email, Length, Regexp
 from datetime import datetime
@@ -12,11 +12,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YourSecretKey'
 
 rooms = [
-    ('101', 'Single Room'),
-    ('102', 'Double Room'),
-    ('103', 'Deluxe Room')
+    ('100', 'Single Room'),
+    ('101', 'Double Room'),
+    ('102', 'Deluxe Room')
 ]
-
 
 
 class BookingForm(FlaskForm):
@@ -25,15 +24,15 @@ class BookingForm(FlaskForm):
     check_out_date = DateField('Check-Out Date', format='%Y-%m-%d', validators=[DataRequired()])
 
 class SignUpForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = EmailField('Email', validators=[DataRequired()])
     password = StringField('Password', widget=PasswordInput(), validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     first_name = StringField('First Name', validators=[DataRequired()])
-    phone_number = StringField('Phone Number', validators=[DataRequired(), Length(min=10, max=10), Regexp(regex='^[0-9]+$')])
+    phone_number = StringField('Phone', validators=[DataRequired(), Length(min=10, max=10), Regexp(regex='^[0-9]+$')])
     submit = SubmitField('Sign Up')
     
 class LogInForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = EmailField('Email', validators=[DataRequired()])
     password = StringField('Password', widget=PasswordInput(), validators=[DataRequired()])
     submit = SubmitField('Log In')
 
@@ -42,14 +41,18 @@ class LogInForm(FlaskForm):
 def index():
     form=BookingForm()
     if form.validate_on_submit():
+            
         if form.check_in_date.data > form.check_out_date.data or form.check_in_date.data < datetime.today().date():
             return render_template('index.html', form=form, error="Check-in or Check-out date not valid !")
         
-        # if check_rooms_available():
-        #     return render_template('index.html', form=form, error="Rooms are available !")
+        if session['id_guest'] == None:
+            return render_template('index.html', form=form, error="You are not connected !")
         
-        book_room(1,form.room_number.data, form.check_in_date.data.strftime('%Y-%m-%d'), form.check_out_date.data.strftime('%Y-%m-%d'))
-        
+        response = book_room(1,form.room_number.data, form.check_in_date.data.strftime('%Y-%m-%d'), form.check_out_date.data.strftime('%Y-%m-%d'))
+        if response['success']:
+            return redirect(url_for('mybookings'))
+        else:
+            return render_template('index.html', form=form, error=response['error'])
 
     return render_template('index.html', form=form)
 
@@ -57,8 +60,14 @@ def index():
 def login():
     form = LogInForm()
     if form.validate_on_submit():
-        log_in(form.email.data, form.password.data)
-        return redirect(url_for('index'))
+        response = log_in(form.email.data, form.password.data)
+        # print(response['data'])
+        if response['success']:
+            session['id_guest'] = response['data']['id_guest'] 
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', form=form, error=response['error'])
+            
     return render_template('login.html', form=form)
 
 
@@ -66,16 +75,29 @@ def login():
 def signup():
     form = SignUpForm()
     if form.validate_on_submit():
-        sign_up(form.email.data, form.password.data, form.last_name.data, form.first_name.data, form.phone_number.data)
-        return redirect(url_for('login'))
+        response = sign_up(form.email.data, form.password.data, form.last_name.data, form.first_name.data, form.phone_number.data)
+        if response['success']:
+            return redirect(url_for('login'))
+        else:
+            return render_template('signup.html', form=form, error=response['error'])
     return render_template('signup.html', form=form)
 
 @app.route('/mybookings')
 def mybookings():
-    return render_template('mybookings.html')
+    response = get_bookings(session['id_guest'])
+    # print(response['data'])
+    if response['success']:
+        return render_template('mybookings.html', data=response['data']['bookings'], rooms={room[0]: room[1] for room in rooms})
+    else:
+        return render_template('mybookings.html', error=response['error'])
+        
 
+@app.route('/logout')
+def logout():
+    session.pop('id_guest', None)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
     bdd_init()
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host='localhost', port=5000, debug=True)
--- src/bdd_requests.py
+++ src/bdd_requests.py
@@ -14,26 +14,10 @@ def bdd_init():
     except requests.exceptions.RequestException as err:
         print(f"Error: {err}")
         
-def check_rooms_available():
-    url = "http://localhost:5001/api/rooms/available"
-    headers = {"Content-Type": "application/json"}
 
-    try:
-        response = requests.post(url, headers=headers)
-        response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
-
-        result = response.json()
-        print(result)
-        return True
-
-    except requests.exceptions.RequestException as err:
-        print(f"Error: {err}")
-        return False
-        
 def book_room(id_guest, id_room, check_in_date, check_out_date):
     url = "http://localhost:5001/api/booking"
     headers = {"Content-Type": "application/json"}
-    print(check_in_date)
     data = {
         "check_in_date": check_in_date,
         "check_out_date": check_out_date,
@@ -46,12 +30,29 @@ def book_room(id_guest, id_room, check_in_date, check_out_date):
         response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
 
         result = response.json()
-        return result
+        return {"success": True, "data": result}
 
     except requests.exceptions.RequestException as err:
-        print(f"Error: {err}")
         # Print additional details about the error if available
-        print(f"Response content: {response.content}")
+        # print(f"Response content: {response.content}")
+        error_message = f"Error: {err}"
+        return {"success": False, "error": error_message}
+    
+def cancel_room(id_booking):
+    url = f"http://localhost:5001/api/booking/{id_booking}"
+
+    try:
+        response = requests.delete(url)  # Use json parameter for JSON data
+        response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+
+        result = response.json()
+        return {"success": True, "data": result}
+
+    except requests.exceptions.RequestException as err:
+        # Print additional details about the error if available
+        # print(f"Response content: {response.content}")
+        error_message = f"Error: {err}"
+        return {"success": False, "error": error_message}
         
 def sign_up(email, password, last_name, first_name, phone_number):
     url = "http://localhost:5001/api/signup"
@@ -70,12 +71,13 @@ def sign_up(email, password, last_name, first_name, phone_number):
         response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
 
         result = response.json()
-        print(result)
+        return {"success": True, "data": result}
 
     except requests.exceptions.RequestException as err:
-        print(f"Error: {err}")
         # Print additional details about the error if available
-        print(f"Response content: {response.content}")
+        # print(f"Response content: {response.content}")
+        error_message = f"Error: {err}"
+        return {"success": False, "error": error_message}
         
 def log_in(email, password):
     url = "http://localhost:5001/api/login"
@@ -91,11 +93,27 @@ def log_in(email, password):
         response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
 
         result = response.json()
-        print(result)
+        return {"success": True, "data": result}
 
     except requests.exceptions.RequestException as err:
-        print(f"Error: {err}")
         # Print additional details about the error if available
-        print(f"Response content: {response.content}")
+        # print(f"Response content: {response.content}")
+        error_message = f"Error: {err}"
+        return {"success": False, "error": error_message}
         
-        
+def get_bookings(id_guest):
+    url = f"http://localhost:5001/api/bookings/{id_guest}"
+    headers = {"Content-Type": "application/json"}
+    
+    try:
+        response = requests.get(url, headers=headers)  # Use json parameter for JSON data
+        response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+
+        result = response.json()
+        return {"success": True, "data": result}
+
+    except requests.exceptions.RequestException as err:
+        # Print additional details about the error if available
+        # print(f"Response content: {response.content}")
+        error_message = f"Error: {err}"
+        return {"success": False, "error": error_message}
